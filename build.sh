17523c388fa40eb09a563db3aef4bba92b842bb3
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2023-08-28 17:09:23 +0800

    尝试修复 GitHub Workflow 报错 x3

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bis
   libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz \
   mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pyelftools \
   libpython3-dev qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip \
-  vim wget xmlto xxd zlib1g-dev
+  vim wget xmlto xxd zlib1g-dev re2c
 
 export OP_BUILD_PATH=$PWD
 git clone https://github.com/coolsnowwolf/lede.git
